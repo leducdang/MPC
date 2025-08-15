44a4fe638df8e38a520bed7801deb75186b60aaa
leducdang <93851712+leducdang@users.noreply.github.com>
2025-08-15 11:11:02 +0700

    Add files via upload
    code on Raspberry

---
+++ TCPsendData.py
@@ -0,0 +1,42 @@
+import socket
+import cv2
+import pickle
+import struct
+import time
+import random  # chỉ dùng để mô phỏng vận tốc
+from interface_I2C import send_two_floats, read_float
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.0.117', 8485))
+# client_socket.connect(('192.168.1.5', 8485))
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    frame = cv2.flip(frame, -1)
+    frame = cv2.resize(frame, (320, 240))
+
+    # velocity = random.uniform(-1.0, 1.0)
+    # velocity = 0.0
+    velocity = read_float()
+
+    data = pickle.dumps((frame, velocity))
+
+    message = struct.pack("Q", len(data)) + data
+
+    client_socket.sendall(message)
+
+    time.sleep(0.075)  # Giới hạn gửi ~20 fps
+
+    
+    received = client_socket.recv(8)  # 2 floats = 4 bytes x 2 = 8 bytes
+    if len(received) == 8:
+        a, theta = struct.unpack('ff', received)
+        print(f"Received a = {a:.2f}, theta = {theta:.2f}")
+        send_two_floats(a, theta)
+        # send_one_floats(theta)
+    
+    
+
+# sudo python3 /home/ubuntu/Code/TCP_testdata.py
---
+++ i2cInterface.py
@@ -0,0 +1,43 @@
+import smbus
+import time
+import struct
+
+bus = smbus.SMBus(1)
+address = 0x08
+
+def send_two_floats(f1, f2):
+    data = struct.pack('ff', f1, f2)
+    byte_list = list(data)
+    bus.write_i2c_block_data(address, 0x00, byte_list)  # gửi nguyên 8 byte 1 lần
+    # for i in range(len(byte_list)):
+    #     bus.write_byte(address, byte_list[i])
+def read_float():
+    try:
+        data = bus.read_i2c_block_data(address, 0, 4)  # đọc 4 byte
+        # Chuyển thành float
+        float_val = struct.unpack('f', bytes(data))[0]
+        return float_val
+    except Exception as e:
+        print("Read error:", e)
+        return None
+
+# def send_one_floats(f1):
+#     data = struct.pack('f', f1)
+#     byte_list = list(data)
+#     # bus.write_i2c_block_data(address, 0x00, byte_list)  # gửi nguyên 8 byte 1 lần
+#     for i in range(len(byte_list)):
+#         bus.write_byte(0x09, byte_list[i])
+     
+while True:
+
+    send_two_floats(3.14, 0.5)
+#     # bus.write_byte(address, byte_list[i])
+    time.sleep(0.5)
+#     #print("Sent: 3.14, 2.718")
+    value = read_float()
+    print(value)
+    # # send_one_floats(-0.5)
+    time.sleep(0.5)
+
+
+# sudo python3 /home/ubuntu/Code/interface_I2C.py
